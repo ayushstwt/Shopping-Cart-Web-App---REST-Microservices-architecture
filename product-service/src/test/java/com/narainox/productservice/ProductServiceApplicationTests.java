ba40ca57b2ff8477cc8c8916cce8cdc4df0fb9e8
Ayush Shrivastava <ayusrivastava2k3@gmail.com>
2024-02-06 19:09:07 +0530

    Product Service Completed

--- product-service/src/test/java/com/narainox/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/narainox/productservice/ProductServiceApplicationTests.java
@@ -2,9 +2,14 @@ package com.narainox.productservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 class ProductServiceApplicationTests {
+
+
 
 	@Test
 	void contextLoads() {
